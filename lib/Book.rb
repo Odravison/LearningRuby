e919d3145a5b9b120af97d04594b8edf9a73df97
Odravison Amaral <odravison.amaral@dce.ufpb.br>
2016-08-01 21:46:58 -0300

    Learning about variable and your scope. Well as convenctions over configurations (CoC).

--- lib/Book.rb
+++ lib/Book.rb
@@ -1,6 +1,19 @@
-# encoding: utf-8
+# coding: utf-8
 class Book
-  def initialize(author, number_of_pages, isbn_code)
-    puts "Author: #{author}, ISBN: #{isbn_code}, Pages: #{number_of_pages}"
+
+  attr_accessor :preco
+  
+  def initialize(author, number_of_pages, isbn_code, preco)
+    @author = author
+    @isbn_code = isbn_code
+    @number_of_pages = number_of_pages
+    @preco = preco
   end
+
+  def to_s
+    "Author: #{@author}, ISBN: #{@isbn_code}, Pages: #{@number_of_pages},
+    Preco: #{@preco}"
+  end
+
+
 end
