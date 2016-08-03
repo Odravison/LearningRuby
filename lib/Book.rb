519bb75e23d91c559c15d018269c58d0b0a09be2
Odravison Amaral <odravison.amaral@dce.ufpb.br>
2016-08-03 12:52:57 -0300

    Using for to interate in Array Object

--- lib/Book.rb
+++ lib/Book.rb
@@ -1,19 +1,20 @@
 # coding: utf-8
 class Book
 
-  attr_accessor :preco
-  
-  def initialize(author, number_of_pages, isbn_code, preco)
+  attr_accessor :price
+  attr_reader :category
+
+  def initialize(author, number_of_pages, isbn_code, price, category)
     @author = author
     @isbn_code = isbn_code
     @number_of_pages = number_of_pages
-    @preco = preco
+    @price = price
+    @category = category
   end
 
   def to_s
     "Author: #{@author}, ISBN: #{@isbn_code}, Pages: #{@number_of_pages},
-    Preco: #{@preco}"
+    Price: #{@price}, category: #{@category}"
   end
-
 
 end
---
+++ lib/Library.rb
@@ -0,0 +1,17 @@
+class Library
+
+  attr_reader :books
+
+  def initialize
+    @books = {} #Inicializa um hash
+  end
+
+  def addBook(book)
+    @books[book.category] ||= []
+    @books[book.category] << book
+  end
+
+  def books
+    @books.values.flatten
+  end
+end
